3faea12b06f3668c44586606b3298a06b6c65d68
dani-doni <128325494+dani-doni@users.noreply.github.com>
2023-07-05 14:25:02 +0200

    Update pop_istat.py

--- pop_istat.py
+++ pop_istat.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 # libraries imported
 
 url_CL_ITTER107 = 'https://raw.githubusercontent.com/ondata/guida-api-istat/master/processing/DCIS_POPRES1/3_Dimension_CL_ITTER107.csv'
@@ -9,4 +10,16 @@ CL_ITTER107 = pd.read_csv(url_CL_ITTER107)
 chosen_ITTER107 = st.selectbox('Comune', options=list(CL_ITTER107['dimensionValueDescription']))
 #choose a ITTER107 from streamlit frontend
 
-print ("hai scelto il comune:" + chosen_ITTER107 )
+FREQ = "A"
+ETA = "TOTAL"
+ITTER107 = chosen_ITTER107
+SESSO = 9
+STACIVX = 99
+TIPO_INDDEM = "JAN"
+#declare API varibles
+
+api_url = "https://sdmx.istat.it/SDMXWS/rest/data/22_289/{}.{}.{}.{}.{}.{}/"
+url = api_url.format (FREQ,ETA,ITTER107,SESSO,STACIVX,TIPO_INDDEM)
+print (url)
+response = requests.get(url)
+response.text
