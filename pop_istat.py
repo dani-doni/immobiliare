196fdbb1e2e7a70a64a6ea23dac46b0b740d5cfb
dani-doni <128325494+dani-doni@users.noreply.github.com>
2023-07-05 15:44:45 +0200

    Update pop_istat.py

--- pop_istat.py
+++ pop_istat.py
@@ -17,9 +17,10 @@ url_CL_ITTER107 = 'https://raw.githubusercontent.com/ondata/guida-api-istat/mast
 CL_ITTER107 = pd.read_csv(url_CL_ITTER107)
 # take data of ITTER107 and read it as csv
 
-
-chosen_ITTER107 = st.selectbox('Comune', options=list(CL_ITTER107['dimensionValueDescription']))
-id_ITTER107 = CL_ITTER107.loc[CL_ITTER107 ['dimensionValueDescription'] == chosen_ITTER107,'dimensionValueID'].iloc[0]
+with st.form("my_form"):
+    chosen_ITTER107 = st.selectbox('Comune', options=list(CL_ITTER107['dimensionValueDescription']))
+    id_ITTER107 = CL_ITTER107.loc[CL_ITTER107 ['dimensionValueDescription'] == chosen_ITTER107,'dimensionValueID'].iloc[0]
+    submitted = st.form_submit_button("Submit")
 #choose a ITTER107 from streamlit frontend and take the ID
 
 FREQ = "A"
@@ -30,23 +31,14 @@ STACIVX = 99
 TIPO_INDDEM = "JAN"
 #declare API varibles
 
-st.write(f"tutto bene")
-st.write(FREQ)
-
 api_url = "https://sdmx.istat.it/SDMXWS/rest/data/22_289/{}.{}.{}.{}.{}.{}/"
 url = api_url.format (FREQ,ETA,ITTER107,SESSO,STACIVX,TIPO_INDDEM)
 st.write(url)
 
-
-with st.form("my_form"):
-    index = st.number_input("ID", min_value=0, max_value=100, key="index")
-    submitted = st.form_submit_button("Submit")
-
 if submitted:
-    st.write("Result")
-    data = fetch(session, f"https://picsum.photos/id/{index}/info")
+    data = fetch(session, url)
     if data:
-        st.image(data['download_url'], caption=f"Author: {data['author']}")
+        st.write("ok")
     else:
         st.error("Error")
       
